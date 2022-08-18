1deee5ffab82d5f9a2f2874b7d0a363c14fe981a
brianjrok <brianjrok@gmail.com>
2022-08-17 21:10:31 -0500

    copied reddit logo with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return <div>Hello World</div>;
-  }
+import Nav from './components/Nav';
+
+const App = () => {
+  return (
+    <Router>
+      <Nav />
+    </Router>
+  )
 }
 
 export default App;
