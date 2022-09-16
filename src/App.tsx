b489cf0770f53e54ed526e78d68ef4a59a4a985f
brianjrok <brianjrok@gmail.com>
2022-09-16 17:18:55 -0600

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Nav from './components/Nav';
 import Login from './components/Login';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
